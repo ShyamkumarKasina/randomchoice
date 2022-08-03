101239a6700bd2b044b7597819c7e0bca8678cfd
Shyam <2100032187@kluniversity.in>
2022-08-03 12:05:12 +0530

    Search.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+n=random.randbytes(3)
+print(n)
+print(random.randrange(1,8))
+print(random.randint(100,211))
+mylist=["jadeja","Ashwin","Shami","dhoni","virat"]
+mylist1={"jadeja","Ashwin","Shami","dhoni","virat"}
+print(random.choice(mylist))
+random.shuffle(mylist)
+print(mylist)
